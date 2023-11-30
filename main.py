6167ad1d1f658287bfb8543605a9b42610843f8b
willswats <willswats@users.noreply.github.com>
2023-11-30 14:26:59 +0000

    Add messy correctly draw_patch

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-from graphics import GraphWin, Point, Text, Rectangle
+from graphics import GraphWin, Point, Text, Line
 
 
-def draw_patch(win, x, y, colour):
+def draw_patch(win, colour):
     size = 20
-    initial_x = x
+    initial_x = 0
 
+    x = 0
+    y = 0
     for _ in range(5):
         for _ in range(5):
-            top_left = Point(x, y)
-            bottom_right = Point(x + size, y + size)
-
-            rectangle = Rectangle(top_left, bottom_right)
-            rectangle.setOutline(colour)
-            rectangle.draw(win)
-
             text_point = Point(x + (size / 2), y + (size / 2))
             text = Text(text_point, "hi!")
             text.setFill(colour)
@@ -24,15 +19,24 @@ def draw_patch(win, x, y, colour):
         x = initial_x
         y += size
 
+    y = 20
+    for _ in range(4):
+        line_row = Line(Point(0, y), Point(100, y))
+        line_row.setOutline(colour)
+        line_row.draw(win)
+        y += 20
+
+    x = 20
+    for _ in range(4):
+        line_row = Line(Point(x, 0), Point(x, 100))
+        line_row.setOutline(colour)
+        line_row.draw(win)
+        x += 20
+
 
 def draw_patchwork():
     win = GraphWin("Draw Patch", 300, 200)
-    draw_patch(win, 0, 0, "blue")
-    draw_patch(win, 100, 0, "blue")
-    draw_patch(win, 200, 0, "blue")
-    draw_patch(win, 0, 100, "blue")
-    draw_patch(win, 100, 100, "blue")
-    draw_patch(win, 200, 100, "blue")
+    draw_patch(win, "red")
     win.getMouse()
 
 
